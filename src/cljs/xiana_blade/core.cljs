65e0060202be2b1a7db2ad77529eb2b987488fce
Gustavo Valente <gustavomsvalente@gmail.com>
2023-03-29 20:39:14 -0300

    Add babel with buttons example

--- src/cljs/xiana_blade/core.cljs
+++ src/cljs/xiana_blade/core.cljs
@@ -2,16 +2,22 @@
   (:require
     [goog.dom :as gdom]
     [reagent.core :as r]
-    [reagent.dom.client :as client]))
-
+    [reagent.dom.client :as client]
+    ["/buttons" :as btns]))
 
 (defonce root (client/create-root (gdom/getElement "root")))
+
+(def buttons (r/adapt-react-class btns/default))
+
+(defn page
+  []
+  [buttons])
 
 
 (defn ^:dev/after-load render!
   "New react18 render"
   []
-  (.render root (r/as-element [:h3 "XIANA-BLADE"])))
+  (.render root (r/as-element [page])))
 
 
 (defn init
