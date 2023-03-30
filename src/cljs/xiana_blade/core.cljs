b3d909638b7818f5ab3d890ef470f26c8fb60c93
Gustavo Valente <gustavomsvalente@gmail.com>
2023-03-29 21:01:54 -0300

    Add tailwindcss UI

--- src/cljs/xiana_blade/core.cljs
+++ src/cljs/xiana_blade/core.cljs
@@ -3,15 +3,19 @@
     [goog.dom :as gdom]
     [reagent.core :as r]
     [reagent.dom.client :as client]
+    ["/dark-nav" :as dark]
     ["/buttons" :as btns]))
 
 (defonce root (client/create-root (gdom/getElement "root")))
 
+(def dark-nav (r/adapt-react-class dark/default))
 (def buttons (r/adapt-react-class btns/default))
+
 
 (defn page
   []
-  [buttons])
+  [dark-nav
+   [buttons]])
 
 
 (defn ^:dev/after-load render!
--- src/jsx/buttons.jsx
+++ src/jsx/buttons.jsx
@@ -5,7 +5,7 @@ export default function Example() {
     <>
       <button
         type="button"
-        className="rounded-full bg-white px-2.5 py-1 text-xs font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
+        className="rounded-full bg-red-900 text-white px-2.5 py-1 text-xs font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
       >
         Button text
       </button>
